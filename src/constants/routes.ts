71e2ceef3af61af18bb08bd379bf52a331860982
Siamion Krapivin <homer1996@gmail.com>
2024-12-05 16:06:14 +0100

    feat: refactor color constants to enum and add route constants for navigation

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+export enum ROUTES {
+  LOGIN = "LoginScreen",
+  REGISTER = "RegisterScreen",
+  SIGN_UP = "SignUp",
+  HOME = "HomeScreen",
+  ACCOUNT = "AccountScreen",
+}
+export enum MAIN_ROUTES {
+  TAB_BAR = "TabBar",
+}
+export enum TABS {
+  HOME = "HomeTab",
+  ACCOUNT = "AccountTab",
+}
+export const bottomlessScreens: (string | undefined)[] = [];
+export const headerlessScreens: (string | undefined)[] = [];
