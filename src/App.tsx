e7799c354518d1917f163b9f624cb520cf5fc231
Siamion Krapivin <homer1996@gmail.com>
2024-12-06 09:59:15 +0100

    feat: integrate react-native-config for environment variable management

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {RootNavigator} from "./navigation/navigators";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import {COLORS} from "./constants/colors";
 
+import Config from "react-native-config";
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
 
@@ -13,6 +15,8 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? COLORS.BLACK : COLORS.WHITE,
     flex: 1,
   };
+
+  console.log("ENVIRONMENT", Config.API_BASE_URL);
 
   return (
     <GestureHandlerRootView style={styles.fullScreen}>
