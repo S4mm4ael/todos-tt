52d597d54c66ed9aa15a28b2ae408ad9dc59eb16
Siamion Krapivin <homer1996@gmail.com>
2024-12-06 13:46:58 +0100

    feat: add ClosedEyeIcon and OpenedEyeIcon SVG components

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,5 @@
 export {AccountIcon} from "./AccountIcon";
 export {HomeIcon} from "./HomeIcon";
 export {ArrowIcon} from "./ArrowIcon";
+export {ClosedEyeIcon} from "./ClosedEyeIcon";
+export {OpenedEyeIcon} from "./OpenedEyeIcon";
